3796ef24b175bf3b1a9d2166d65a60aa67544756
Graham Guletz <grguletz@gmail.com>
2013-12-03 22:50:05 -0800

    Update Problem1.m

--- Homework4/Problem1.m
+++ Homework4/Problem1.m
@@ -1,91 +1,93 @@
-%Problem 1
+%Problem 2
 %Ramon Rovirosa and Graham Guletz
 
-function [a] = Problem1()
-    scrsz = get(0,'ScreenSize');
+function [a] = Problem2()
+scrsz = get(0,'ScreenSize');
     
-    %Condition where c=-1
-    c=1;
     m=100;%number of grid points
-    a=-1;
-    b=1;
+    a=-pi/2;
+    b=pi/2;
     t=0;
-    t_f=2;
+    t_f=pi;
     x = linspace(a, b, m);
+    y = linspace(a, b, m);
     dx = x(2)-x(1);
-    dt=.5*dx;
-    U=zeros(m);
-    U=cos(pi*x);
+    dy = y(2)-y(1);
+    dt=.2*dx;
+    u=zeros(m, m);
+    u_prev=zeros(m, m);
+    v=zeros(m, m);
+    v_prev=zeros(m, m);
+    rho=zeros(m,m);
+    
+    %populate initial rho
+    for i=1:m
+       for j=1:m
+           if(sqrt((x(i)-1)*(x(i)-1)+y(j)*y(j))<.25)
+               rho(i,j)=1;
+           else
+               rho(i,j)=0;
+           end
+       end
+    end
 
+    %boundary conditions
+    %u(-pi/2, :)=0;
+    %u(pi/2, :)=0;
+    %v(:, -pi/2)=0;
+    %v(:, pi/2)=0;
+       
     figure('Position',[0 scrsz(4)/3 scrsz(3)/2 scrsz(4)/2]);
-    axis([a b a b]);
+    %axis([a b a b]);
     
     while(t<t_f)
-        %t=t_f;
-        U_prev=U;
-        for i=2:m
-            U=U_prev-c*(dt/dx)*(U_prev(i)-U_prev(i-1));
+        mesh(x,y, rho);
+        axis ([-pi/2 pi/2 -pi/2 pi/2 0 1 ]);
+        pause(.05);
+        
+        for i=1:m
+           for j=1:m
+              u(i,j)=-cos(x(i))*sin(y(j))*cos(t);
+              v(i,j)=sin(x(i))*cos(y(j))*cos(t);
+           end
         end
-        U(1)=U_prev(1)-c*(dt/dx)*(U_prev(1)-U_prev(m));
-         t=t+dt;
+        t=t+dt;
+        rho_old=rho;
+        for i=2:m-1
+           for j=2:m-1
+              
+                  if(v(i,j)<=0)
+                          rho(i,j) = rho(i,j) - v(i,j)*(rho_old(i,j+1)-rho_old(i,j))/.5;
+                  else
+                          rho(i,j) = rho(i,j) - v(i,j)*(rho_old(i,j)-rho_old(i,j-1));
+                  end
+                  if(u(i,j)<=0)
+                          rho(i,j) = rho(i,j) - u(i,j)*(rho_old(i+1,j)-rho_old(i,j));
+                  else
+                          rho(i,j) = rho(i,j) - u(i,j)*(rho_old(i,j)-rho_old(i-1,j));
+                  end
+           end
+           
+        end
+       
     end
 
-    plot(U, 'b');
-    hold on;
+    %figure('Position',[scrsz(4)/3 scrsz(4)/3 scrsz(3)/2 scrsz(4)/2]);
+    %g=quiver(x, y, u, v, 'Color', 'black');
+    %drawnow;
+    %refreshdata;
+    %mesh(x, y, rho);
+    %plot(rho, 'b');
+    %hold on;
     
     
-    T=zeros(0, 100);
-    for i=1:100
-        T(i)=cos(pi*(x(i)-2));
-        i=i+1;
-    end
-    plot(T,'r');
-    title('Plot for c=1');
-    legend('Numerical', 'Exact');
-    set(gca, 'XTickLabel', -1:.2:1);
-    hold off;
-    
-    %Condition where c=-1
-    clc;
-    c=-1;
-    m=100;%number of grid points
-    a=-1;
-    b=1;
-    t=0;
-    t_f=2;
-    x = linspace(a, b, m);
-    dx = x(2)-x(1);
-    dt=.5*dx;
-    
-    figure('Position',[scrsz(3)/2 scrsz(4)/3 scrsz(3)/2 scrsz(4)/2]);
-    axis([a b a b]);
-
-    U=zeros(m);
-    U=cos(pi*x);
-    
-    while(t<t_f)
-       %t=t_f;
-        U_prev=U;
-        for i=1:m-1
-            U=U_prev-c*(dt/dx)*(U_prev(i+1)-U_prev(i));
-        end
-        U(m)=U_prev(m)-c*(dt/dx)*(U_prev(1)-U_prev(m));
-         t=t+dt;
-    end
-    plot(U, 'b');
-    hold on;
     
     
-    T=zeros(0, 100);
-    for i=1:100
-        T(i)=cos(pi*(x(i)-2));
-        i=i+1;
-    end
-    plot(T,'r');
-    title('Plot for c=-1');
-    legend('Numerical', 'Exact');
-    set(gca, 'XTickLabel', -1:.2:1);
-    hold off;
+    title('Plot for 2D Upwind Scheme');
+    %legend('Numerical');
+    set(gca, 'XTickLabel', -pi/2:dx:pi/2);
+    %hold off;
+    
     
     
 end
